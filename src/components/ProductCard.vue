d8c223cd9cb96ee01053da4096dfba43b479ffcb
istamovv <istamov.valeriy@wildberries.work>
2023-10-31 17:25:53 +0300

    card redesign, remove dafault card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,31 +3,28 @@
     <v-card v-bind="hoverProps"
             variant="outlined"
             style="border: 1px solid white">
-      <div @click="goToProductPage"
-           class="product-card">
-        <div class="marks">
-          <svg class="fav-mark" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#d0d0d0" viewBox="0 0 256 256">
-            <path
-              d="M240 98a57.63 57.63 0 0 1-17 41l-89.3 90.62a8 8 0 0 1-11.4 0L33 139a58 58 0 0 1 82-82.1l13 12.15l13.09-12.19A58 58 0 0 1 240 98Z"/>
-          </svg>
-          <div class="discount-mark" v-if="discount !== 0">скидка {{ discount }}%</div>
-        </div>
+      <div class="product-card">
         <div class="img-holder">
+          <svg class="fav-mark"
+               xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 256 256">
+            <path
+                d="M223 57a58.07 58.07 0 0 0-81.92-.1L128 69.05l-13.09-12.19A58 58 0 0 0 33 139l89.35 90.66a8 8 0 0 0 11.4 0L223 139a58 58 0 0 0 0-82Zm-11.35 70.76L128 212.6l-83.7-84.92a42 42 0 0 1 59.4-59.4l.2.2l18.65 17.35a8 8 0 0 0 10.9 0l18.65-17.35l.2-.2a42 42 0 1 1 59.36 59.44Z"/>
+          </svg>
+          <div class="discount-mark" v-if="discount !== 0">- {{ discount }}%</div>
           <img :src="mainImage" class="product-img" alt="product-img"/>
         </div>
-        <div class="name-stock">
-          <p class="product-name">{{ name }}</p>
-          <p :class="`${props.stock === 1 ? 'in-stock' : 'on-order'}`">{{
+        <div class="price">
+          <p style="font-size: 1.2rem">{{ price }} <span class="rub">Р</span></p>
+          <p v-if="discount !== 0" class="old-price">
+            {{ countDiscount }}
+            <span class="rub">Р</span></p>
+        </div>
+        <p class="product-name">{{ name }}</p>
+        <div class="cart-stock">
+          <button class="cart-btn">В корзину</button>
+          <p class="stock">{{
               props.stock === 1 ? 'В наличии' : 'Под заказ'
             }}</p>
-        </div>
-        <div class="price-cart">
-          <div class="price">
-            <p v-if="discount !== 0" style="text-decoration: line-through; font-size: 0.9rem">{{ countDiscount }}
-              руб/шт.</p>
-            <p style="color: #E3DD5F; font-weight: bold">{{ price }} руб/шт.</p>
-          </div>
-          <button class="cart-btn">В корзину</button>
         </div>
       </div>
       <v-overlay :model-value="isHovering"
@@ -100,72 +97,97 @@ const goToProductPage = () => {
   flex-direction: column
   justify-content: space-evenly
 
+*
+  color: black
+
 .product-card
+  gap: 10px
   display: flex
   flex-direction: column
 
-  width: 250px
-  height: 355px
-  padding-bottom: 10px
+  z-index: 5
+  height: 380px
+  cursor: pointer
+  width: 235px
   position: relative
-
-  .marks
-    left: 0
-    padding: 2px
-    position: absolute
-
-    width: 100%
-    display: flex
-    align-items: center
-    box-sizing: border-box
-    flex-direction: row-reverse
-    justify-content: space-between
-
-    .discount-mark
-      padding: 0 5px
-      border-radius: 5%
-      font-size: 0.9rem
-      color: #555555
-      height: fit-content
-      background-color: #E3DD5F
+  background-color: white
 
   .img-holder
     width: 100%
-    height: 200px
+    min-height: 235px
     display: flex
-    margin-top: 25px
-    align-items: center
-    justify-content: center
+    position: relative
 
     img
-      max-width: 100%
-      max-height: 200px
+      width: 75%
+      margin: auto
+      object-fit: cover
 
-  .name-stock
-    margin: 10px
+    &::after
+      top: 0
+      content: ''
+      height: 100%
+      width: 100%
+      position: absolute
+      border-radius: 15px
+      background-color: rgba(0, 48, 120, 0.04)
 
-    .product-name
-      overflow: hidden
-      display: -webkit-box
-      -webkit-line-clamp: 2
-      -webkit-box-orient: vertical
+    .discount-mark
+      top: 5px
+      left: 5px
+      font-size: 14px
+      padding: 3px 10px
+      text-align: center
+      position: absolute
+      border-radius: 12px
+      background-color: #E3DD5F
 
-  p.in-stock
-    font-weight: bold
-    color: #49AE66
+    .fav-mark
+      right: 3px
+      bottom: 3px
+      cursor: pointer
+      position: absolute
+      fill: #808080
 
-  .price-cart
+  .price
+    gap: 10px
+    display: flex
+
+    .old-price
+      font-size: 0.9rem
+      color: #808080
+      text-decoration: line-through
+
+    p > .rub
+      width: 0.5em
+      color: inherit
+      line-height: 0.2em
+      display: inline-block
+      vertical-align: middle
+      text-decoration: inherit
+      border-bottom: 0.07em solid
+
+  .product-name
+    overflow: hidden
+    font-weight: lighter
+    display: -webkit-box
+    -webkit-line-clamp: 2
+    -webkit-box-orient: vertical
+
+  .cart-stock
     display: flex
     margin-top: auto
-    align-items: flex-end
-    justify-content: space-evenly
+    align-items: center
+    justify-content: space-between
+
+    .stock
+      font-weight: lighter
 
     .cart-btn
       border: none
       cursor: pointer
-      padding: 5px 10px
+      padding: 5px 20px
       color: #555555
-      min-width: 40%
-      border-radius: 5px
+      border-radius: 12px
       background-color: #E3DD5F
 </style>
--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,29 +1,31 @@
 <template>
   <router-view>
-  <h2>{{ text }}</h2>
-  <div class="cards">
-    <default-card class="card" :category-name="text"/>
-    <product-card
-      v-for="[id, product] in useProductsStore().products"
-      :key="id"
-      :id="product.id"
-      :name="product.name"
-      :images="product.images"
-      :price="product.price"
-      :stock="product.stock"
-      :discount="product.discount"
-      :category-name="text"
-    />
-  </div>
+    <h2>{{ text }}</h2>
+    <v-btn variant="flat" prepend-icon="mdi-plus" color="#E3DD5F" class="mb-8"
+           @click.stop="router.push({name: 'Creation', params: { text: text }})">ДОБАВИТЬ ТОВАР
+    </v-btn>
+    <div class="cards">
+      <product-card
+          v-for="[id, product] in useProductsStore().products"
+          :key="id"
+          :id="product.id"
+          :name="product.name"
+          :images="product.images"
+          :price="product.price"
+          :stock="product.stock"
+          :discount="product.discount"
+          :category-name="text"
+      />
+    </div>
   </router-view>
 </template>
 
 <script setup lang="ts">
-import DefaultCard from "@/components/DefaultCard.vue";
 import { useRoute } from "vue-router";
 import { useProductsStore } from "@/store/products";
 import ProductCard from "@/components/ProductCard.vue";
 import { useCategoriesBrandsStore } from "@/store/categories-brands";
+import router from "@/router";
 
 const route = useRoute()
 const text = route.params.text.toString()
@@ -38,5 +40,4 @@ useProductsStore().loadAll(0, 30, categoryId)
   gap: 2rem 1rem
   display: flex
   flex-wrap: wrap
-  justify-content: center
 </style>
